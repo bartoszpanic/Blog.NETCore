a8029efc8361665f0bc53a0c72633e7e383616c2
Bartosz Panic <bartosz.panic@gmail.com>
2021-11-08 20:03:39 +0100

    Add comment and post repository

---
+++ Blog.NETCore.Application/Interfaces/ICommentRepository.cs
@@ -0,0 +1,11 @@
+﻿using Blog.NETCore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.NETCore.Application.Interfaces
+{
+    public interface ICommentRepository : IAsyncRepository<Comment>
+    {
+    }
+}
