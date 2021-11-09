946fea34c77f2a0287fdcea99833b3e63a877a95
Bartosz Panic <bartosz.panic@gmail.com>
2021-11-09 18:37:06 +0100

    Add mapping profile and post map

---
+++ Blog.NETCore.Application/Mapper/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Blog.NETCore.Application.Functions.Posts.Queries.GetPostsList;
+using Blog.NETCore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.NETCore.Application.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Post, PostInListViewModel>().ReverseMap();
+        }
+    }
+}
