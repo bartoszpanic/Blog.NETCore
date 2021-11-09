203511de6ad06e5d779f9f267cacc14d4b7d5480
Bartosz Panic <bartosz.panic@gmail.com>
2021-11-09 18:40:56 +0100

    implement method list of post handler

--- Blog.NETCore.Application/Functions/Posts/Queries/GetPostsList/GetPostsListQueryHandler.cs
+++ Blog.NETCore.Application/Functions/Posts/Queries/GetPostsList/GetPostsListQueryHandler.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using Blog.NETCore.Application.Interfaces;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,9 +11,17 @@ namespace Blog.NETCore.Application.Functions.Posts.Queries.GetPostsList
 {
     public class GetPostsListQueryHandler : IRequestHandler<GetPostInListQuery, List<PostInListViewModel>>
     {
-        public Task<List<PostInListViewModel>> Handle(GetPostInListQuery request, CancellationToken cancellationToken)
+        private readonly IPostRepository _postRepository;
+        private readonly IMapper _mapper;
+        public GetPostsListQueryHandler(IPostRepository postRepository, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+            _postRepository = postRepository;
+        }
+        public async Task<List<PostInListViewModel>> Handle(GetPostInListQuery request, CancellationToken cancellationToken)
+        {
+            var posts = await _postRepository.GetAllAsync();
+            return _mapper.Map<List<PostInListViewModel>>(posts);
         }
     }
 }
