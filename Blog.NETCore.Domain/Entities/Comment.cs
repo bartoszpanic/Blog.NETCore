981db015db27ea91a3d4328cb7638cb8a7e95fb4
Bartosz Panic <bartosz.panic@gmail.com>
2021-11-09 20:17:24 +0100

    Add method to comment repository

--- Blog.NETCore.Domain/Entities/Comment.cs
+++ Blog.NETCore.Domain/Entities/Comment.cs
@@ -8,5 +8,7 @@ namespace Blog.NETCore.Domain.Entities
     {
         public int CommentId { get; set; }
         public string Content { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; }
     }
 }
