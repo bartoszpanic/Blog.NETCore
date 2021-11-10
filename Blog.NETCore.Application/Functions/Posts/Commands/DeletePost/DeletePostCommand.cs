f1af3357a1ad1690b7a26cbad01a5d74d3919e36
Bartosz Panic <bartosz.panic@gmail.com>
2021-11-10 19:16:25 +0100

    Add delete post command and handler

---
+++ Blog.NETCore.Application/Functions/Posts/Commands/DeletePost/DeletePostCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.NETCore.Application.Functions.Posts.Commands.DeletePost
+{
+    public class DeletePostCommand : IRequest
+    {
+        public int PostId { get; set; }
+    }
+}
---
+++ Blog.NETCore.Application/Functions/Posts/Commands/DeletePost/DeletePostCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Blog.NETCore.Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Blog.NETCore.Application.Functions.Posts.Commands.DeletePost
+{
+    public class DeletePostCommandHandler : IRequestHandler<DeletePostCommand>
+    {
+        private readonly IPostRepository _postRepository;
+
+        public DeletePostCommandHandler(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+        public async Task<Unit> Handle(DeletePostCommand request, CancellationToken cancellationToken)
+        {
+            var postDelete = await _postRepository.GetByIdAsync(request.PostId);
+            await _postRepository.DeleteAsync(postDelete);
+            return Unit.Value;
+        }
+    }
+}
