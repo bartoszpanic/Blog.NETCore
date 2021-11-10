63df0dacd8d2a91eddb668cead2d915abd90a19e
Bartosz Panic <bartosz.panic@gmail.com>
2021-11-10 19:44:06 +0100

    Add create comment command and handler

---
+++ Blog.NETCore.Application/Functions/Comments/Commands/CreatedCommentCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.NETCore.Application.Functions.Comments.Commands
+{
+    public class CreatedCommentCommand : IRequest<int>
+    {
+        public int CommentId { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public int PostId { get; set; }
+    }
+}
---
+++ Blog.NETCore.Application/Functions/Comments/Commands/CreatedCommentCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Blog.NETCore.Application.Interfaces;
+using Blog.NETCore.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Blog.NETCore.Application.Functions.Comments.Commands
+{
+    public class CreatedCommentCommandHandler : IRequestHandler<CreatedCommentCommand, int>
+    {
+        private readonly ICommentRepository _commentRepository;
+        private readonly IMapper _mapper;
+
+        public CreatedCommentCommandHandler(ICommentRepository commentRepository, IMapper mapper)
+        {
+            _commentRepository = commentRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<int> Handle(CreatedCommentCommand request, CancellationToken cancellationToken)
+        {
+            var comment = _mapper.Map<Comment>(request);
+            comment = await _commentRepository.AddAsync(comment);
+
+            return comment.CommentId;
+        }
+    }
+}
