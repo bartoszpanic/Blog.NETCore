00dd39cd0503688830146fce323ee32c0539d29d
Bartosz Panic <bartosz.panic@gmail.com>
2021-11-11 18:30:38 +0100

    Add fluent validation on create post command

---
+++ Blog.NETCore.Application/Functions/Posts/Commands/CreatePost/CreatedPostCommandValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.NETCore.Application.Functions.Posts.Commands.CreatePost
+{
+    public class CreatedPostCommandValidator : AbstractValidator<CreatedPostCommand>
+    {
+        public CreatedPostCommandValidator()
+        {
+            RuleFor(p => p.Title)
+                .NotEmpty()
+                .WithMessage("{PropertyName} is required")
+                .NotNull()
+                .MaximumLength(80)
+                .WithMessage("{PropertyName} must not exceed between 80 characters");
+
+            RuleFor(p => p.Content)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("{PropertyName} is required")
+                .MaximumLength(1000)
+                .WithMessage("{PropertyName} must not exceed between 1000 characters");
+
+            RuleFor(p => p.Description)
+                .NotEmpty()
+                .WithMessage("{PropertyName} is required")
+                .NotNull()
+                .MaximumLength(200)
+                .WithMessage("{PropertyName} must not exceed between 1000 characters");
+        }
+    }
+}
