b4eaf02e728f1fc633b1c9728d323e79a9dca9cc
Bartosz Panic <bartosz.panic@gmail.com>
2021-11-17 08:30:22 +0100

    Add delete post command validator and response

---
+++ Blog.NETCore.Application/Functions/Posts/Commands/DeletePost/DeletePostCommandResponse.cs
@@ -0,0 +1,30 @@
+﻿using Blog.NETCore.Application.Response;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.NETCore.Application.Functions.Posts.Commands.DeletePost
+{
+    public class DeletePostCommandResponse : BaseResponse
+    {
+        public int? PostId { get; set; }
+
+        public DeletePostCommandResponse() : base()
+        {
+        }
+        public DeletePostCommandResponse(string message) : base(message)
+        {
+        }
+        public DeletePostCommandResponse(string message, bool success) : base(message, success)
+        {
+        }
+        public DeletePostCommandResponse(ValidationResult validationResult) : base(validationResult)
+        {
+        }
+        public DeletePostCommandResponse(int postId)
+        {
+            PostId = postId;
+        }
+    }
+}
---
+++ Blog.NETCore.Application/Functions/Posts/Commands/DeletePost/DeletePostCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.NETCore.Application.Functions.Posts.Commands.DeletePost
+{
+    public class DeletePostCommandValidator : AbstractValidator<DeletePostCommand>
+    {
+        public DeletePostCommandValidator()
+        {
+            RuleFor(p => p.PostId)
+                .NotEmpty()
+                .WithMessage("{PropertyName} is required")
+                .NotNull()
+                .WithMessage("{PropertyName} is required");
+        }
+    }
+}
