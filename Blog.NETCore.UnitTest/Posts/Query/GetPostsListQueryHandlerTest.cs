bd881aba4567fbff350beb298ae07ec68a5f6e03
Bartosz Panic <bartosz.panic@gmail.com>
2021-11-22 19:17:52 +0100

    add get post list query test

---
+++ Blog.NETCore.UnitTest/Posts/Query/GetPostsListQueryHandlerTest.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Blog.NETCore.Application.Functions.Posts.Queries.GetPostsList;
+using Blog.NETCore.Application.Interfaces;
+using Blog.NETCore.Application.Mapper;
+using Blog.NETCore.UnitTest.Mocks;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Blog.NETCore.UnitTest.Posts.Query
+{
+    public class GetPostsListQueryHandlerTest
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IPostRepository> _mockRepository;
+
+        public GetPostsListQueryHandlerTest()
+        {
+            _mockRepository = RepositoryMocks.GetPostRepository();
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+            _mapper = configurationProvider.CreateMapper();
+        }
+        [Fact]
+        public async Task GetPostListTest()
+        {
+            var postCount = await _mockRepository.Object.GetAllAsync();
+            var handler = new GetPostsListQueryHandler(_mockRepository.Object, _mapper);
+
+            var result = await handler.Handle(new GetPostInListQuery(), CancellationToken.None);
+
+            result.ShouldBeOfType<GetPostInListQueryResponse>();
+        }
+    }
+}
