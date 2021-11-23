2413e8ae9e4d49e1c69b047409c70f846e223038
Bartosz Panic <bartosz.panic@gmail.com>
2021-11-23 20:32:08 +0100

    add persistence installation

---
+++ Blog.NETCore.Persistence.EF/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Blog.NETCore.Application.Interfaces;
+using Blog.NETCore.Persistence.EF.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.NETCore.Persistence.EF
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BlogNETCoreContext>(options => options.UseSqlServer(configuration.GetConnectionString
+                ("BlogNETCoreConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            services.AddScoped<IPostRepository, PostRepository>();
+
+            return services;
+        }
+    }
+}
