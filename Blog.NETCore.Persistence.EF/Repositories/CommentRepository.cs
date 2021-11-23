274a67cac34cfaa52dbe66ac00c921205d7a5a2c
Bartosz Panic <bartosz.panic@gmail.com>
2021-11-23 19:28:48 +0100

    Add entities repositories

---
+++ Blog.NETCore.Persistence.EF/Repositories/CommentRepository.cs
@@ -0,0 +1,25 @@
+﻿using Blog.NETCore.Application.Interfaces;
+using Blog.NETCore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.NETCore.Persistence.EF.Repositories
+{
+    public class CommentRepository : BaseRepository<Comment>, ICommentRepository
+    {
+        public CommentRepository(BlogNETCoreContext context) : base(context)
+        {
+        }
+        public async Task<List<Comment>> GetAllCommentsFromPost(int postId)
+        {
+            var allComments = await _context.Comments.Include(p => p.Post).ToListAsync();
+            allComments = allComments.Where(p => p.PostId == postId).ToList();
+
+            return allComments;
+        }
+    }
+}
---
+++ Blog.NETCore.Persistence.EF/Repositories/PostRepository.cs
@@ -0,0 +1,15 @@
+﻿using Blog.NETCore.Application.Interfaces;
+using Blog.NETCore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.NETCore.Persistence.EF.Repositories
+{
+    public class PostRepository : BaseRepository<Post>, IPostRepository
+    {
+        public PostRepository(BlogNETCoreContext context) : base(context)
+        {
+        }
+    }
+}
