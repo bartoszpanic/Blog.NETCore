e6a54758e3f287770da63afc3e482352e4ec4857
Bartosz Panic <bartosz.panic@gmail.com>
2021-11-23 17:44:53 +0100

    add ef validation for entities

---
+++ Blog.NETCore.Persistence.EF/Configuration/CommentConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Blog.NETCore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.NETCore.Persistence.EF.Configuration
+{
+    public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.Property(c => c.Author)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            builder.Property(c => c.Content)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(c => c.PostId)
+                .IsRequired();
+        }
+    }
+}
---
+++ Blog.NETCore.Persistence.EF/Configuration/PostConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Blog.NETCore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.NETCore.Persistence.EF.Configuration
+{
+    public class PostConfiguration : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.Property(p => p.Title)
+                .IsRequired()
+                .HasMaxLength(80);
+
+            builder.Property(p => p.Content)
+                .IsRequired()
+                .HasMaxLength(1000);
+
+            builder.Property(p => p.Description)
+                .IsRequired()
+                .HasMaxLength(200);
+        }
+    }
+}
